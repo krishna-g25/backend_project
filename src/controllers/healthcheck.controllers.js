6fa270aa47eb27663493d930246babd2bafa8eef
Krishna Goel <goelkrishna844@gmail.com>
2026-01-15 23:38:15 +0530

    fix: corrected typo in healthcheck route function

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,8 +1,14 @@
-import { ApiResponse } from "../utils/api-response"; 
+import { ApiResponse } from "../utils/api-response.js";
+
 const healthcheck = (req, res) => {
     try {
-        res.status(200).json(
-        new ApiResponse(200, {message:"API is healthy"}));
+        return res.status(200).json(
+            new ApiResponse(200, "OK", "API is healthy") 
+        );
     } catch (error) {
-    }};
-    export {healthcheck};
+        // Always send a response even in catch
+        return res.status(500).json({ success: false, message: error.message });
+    }
+};
+
+export { healthcheck };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import brcypt from "bcrypt";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
 
@@ -66,12 +66,12 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = await brcypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  return await brcypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 userSchema.methods.generateAccessToken = function () {
