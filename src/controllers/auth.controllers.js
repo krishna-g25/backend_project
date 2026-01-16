d4b98005ddc9d85c65f07bdd6d09d0bb63e167b2
Krishna Goel <goelkrishna844@gmail.com>
2026-01-17 02:20:47 +0530

    issue resolved

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,4 @@
-import { User } from "../models/user.models.js";
+import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/api-response.js";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import {Router} from 'express';
-import {registerUser} from '../controllers/auth.controller.js';
+import {registerUser} from '../controllers/Auth.controllers.js'; 
 const router = Router();
-router.route('/register').post (registerUser);
+router.route('/register').post(registerUser);
 
-export default router;      
+export default router;
